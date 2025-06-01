f241a20a601f988b44f51497f943b7090f56e580
zpl38 <contact.kheinc@gmail.com>
2025-06-01 11:12:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -80,8 +80,20 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             "nome": "O Poder do Hábito",
             "link": "https://fir3.net/8nZ16Zg",
-            "foto": "https://m.media-amazon.com/images/I/815iPX0SgkL._SL1500_.jpg",
+            "foto": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn1GcSNHDT2uWU1P7YJSLHxigN7r90EiTUjUfjIBCYzmjOF_18SPcfqQsGmcIq0vVUsYuxiJ-GsuUum2mgYjBFzaLsbFmuZZXuvxlsVNVOO-n5xet0N_aUVHMqtfA",
             "descricao": "Charles Duhigg explora a ciência por trás dos hábitos, explicando como eles são formados e como podem ser transformados para melhorar a produtividade, saúde e bem-estar."
+        },
+        {
+            "nome": "48 Leis Do Poder",
+            "link": "https://fir3.net/Z5H4x",
+            "foto": "https://trechos.org/wp-content/uploads/2021/09/Livro-As-48-Leis-do-Poder-por-Robert-Greene.jpg",
+            "descricao": "Um guia estratégico sobre poder, manipulação e influência, baseado em lições históricas e filosóficas. Robert Greene apresenta 48 leis práticas para navegar por dinâmicas sociais e alcançar o sucesso em ambientes competitivos, com exemplos de figuras históricas."
+        },
+        {
+            "nome": "Mais Esperto Que o Diabo",
+            "link": "https://fir3.net/E0c5L",
+            "foto": "https://livraria.funep.org.br/wp-content/uploads/2021/12/MAIS-ESPERTO-QUE-O-DIABO_THIAGONIGRO_SELO2021.png",
+            "descricao": "Escrito por Napoleon Hill, este livro revela, por meio de uma alegórica conversa com o 'Diabo', os segredos por trás do fracasso e do sucesso. Explora armadilhas mentais que limitam o potencial humano e oferece insights para superá-las e alcançar objetivos."
         }
     ];
 
